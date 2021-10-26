e646dca03cf9f266acdb6fa172b202a0b8e81b21
fikrikhoironn <fikrikf470@gmail.com>
2021-10-26 17:42:22 +0700

    (develop): add adt buildings but not finish yet

---
+++ buildings/buildings.h
@@ -0,0 +1,15 @@
+// File : bangunan.h
+// ADT yang berkaitan dengan objek bangunan
+
+#ifndef buildings_H
+#define buildings_H
+
+#include "boolean.h"
+
+typedef struct{
+    char name;
+}buildings;
+
+#define name(C) (C).name;
+
+#endif
