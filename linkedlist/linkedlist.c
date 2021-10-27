57d9ce01e575ddba2133a983fa431c41a419617c
chryes220 <christinehutabarat12@gmail.com>
2021-10-27 18:55:09 +0700

    (develop) Add ADT linked list

---
+++ linkedlist/linkedlist.c
@@ -0,0 +1,242 @@
+// File: linkedlist.c
+// Berisi implementasi dari primitif struktur list berkait 
+// List dinyatakan melalui alamat elemen pertamanya 
+
+#include "linkedlist.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void CreateList(linkedList *L) {
+    // KAMUS
+    // ALGORITMA
+    first(*L) = NULL;
+}
+
+Address newNode(infotype val) {
+    // KAMUS
+    Address p = (Address) malloc(sizeof(Node));
+    // ALGORITMA
+    if (p!=NULL) {
+        info(p) = val;
+        next(p) = NULL;
+    }
+    return p;
+}
+
+boolean isEmpty(linkedList L) {
+    // KAMUS
+    // ALGORITMA
+    return (first(L) == NULL);
+}
+
+infotype getElmt(linkedList L, int idx) {
+    // KAMUS
+    int count;
+    Address p;
+    // ALGORITMA
+    count = 0;
+    p = first(L);
+    while (count < idx) {
+        p = next(p);
+        count++;
+    }
+    return (info(p));
+}
+
+void setElmt(linkedList *L, int idx, infotype val) {
+    // KAMUS
+    int count;
+    Address p;
+    // ALGORITMA
+    count = 0;
+    p = first(*L);
+    while (count < idx) {
+        p = next(p);
+        count++;
+    }
+    info(p) = val;
+}
+
+int indexOf(linkedList L, infotype val) {
+    // KAMUS
+    int i;
+    Address p;
+    boolean found;
+    // ALGORITMA
+    i = 0;
+    p = first(L);
+    found = false;
+    while (p != NULL && !found) {
+        if (info(p) == val) {
+            found = true;
+        }
+        else {
+            i++;
+            p = next(p);
+        }
+    }
+    if (found) {
+        return i;
+    }
+    else {
+        return IDX_UNDEF;
+    }
+}
+
+int length(linkedList L) {
+    // KAMUS
+    int count;
+    Address p;
+    // ALGORITMA
+    count = 0;
+    p = first(L);
+    while (p != NULL) {
+        count++;
+        p = next(p);
+    }
+    return count;
+}
+
+void insertFirst(linkedList *L, infotype val) {
+    // KAMUS
+    Address p;
+    // ALGORITMA
+    p = newNode(val);
+    if (p != NULL) {
+        next(p) = first(*L);
+        first(*L) = p;
+    }
+}
+
+void insertAt(linkedList *L, int idx, infotype val) {
+    // KAMUS
+    Address pNew, pList;
+    int i;
+    // ALGORITMA
+    if (idx == 0) {
+        insertFirst(L, val);
+    }
+    else {
+        pNew = newNode(val);
+        if (pNew != NULL) {
+            pList = first(*L);
+            for (i = 0; i < idx - 1; i++) {
+                pList = next(pList);
+            }
+            next(pNew) = next(pList);
+            next(pList) = pNew;
+        }
+    }
+}
+
+void insertLast(linkedList *L, infotype val) {
+    // KAMUS
+    Address pNew, pList;
+    // ALGORITMA
+    if (isEmpty(*L)) {
+        insertFirst(L, val);
+    }
+    else {
+        pNew = newNode(val);
+        if (pNew != NULL) {
+            pList = first(*L);
+            while (next(pList) != NULL) {
+                pList = next(pList);
+            }
+            next(pList) = pNew;
+        }
+    }
+}
+
+void deleteFirst(linkedList *L, infotype *val) {
+    // KAMUS
+    Address p;
+    // ALGORITMA
+    p = first(*L);
+    *val = info(p);
+    if (next(p) == NULL) {
+        first(*L) = NULL;
+    }
+    else {
+        first(*L) = next(*L);
+    }
+    free(p);
+}
+
+void deleteAt(linkedList *L, int idx, infotype *val) {
+    // KAMUS
+    Address p, temp;
+    int i;
+    // ALGORITMA
+    if (idx == 0) {
+        deleteFirst(L, val);
+    }
+    else if (idx == length(*L)) {
+        // tidak ada yang dihapus
+    }
+    else {
+        p = first(*L);
+        for (i = 0; i < idx - 1; i++) {
+            p = next(p);
+        }
+        temp = next(p);
+        *val = info(temp);
+        next(p) = next(temp);
+        free(temp);
+    }
+}
+
+void deleteLast(linkedList *L, infotype *val, int idx) {
+    // KAMUS
+    Address p, loc;
+    // ALGORITMA
+    p = first(*L);
+    loc = NULL;
+    while (next(p) != NULL) {
+        loc = p;
+        p = next(p);
+    }
+    if (loc == NULL) {
+        first(*L) = NULL;
+    }
+    else {
+        next(loc) = NULL;
+    }
+    *val = info(p);
+    free(p);
+}
+
+void displayList(linkedList L) {
+    // KAMUS
+    Address p;
+    // ALGORITMA
+    printf("[");
+    if (!isEmpty(L)) {
+        p = first(L);
+        while (next(p) != NULL) {
+            printf("%d,", info(p));
+            p = next(p);
+        }
+        printf("%d", info(p));
+    }
+    printf("]");
+}
+
+linkedList concat(linkedList L1, linkedList L2) {
+    // KAMUS
+    Address p1, p2;
+    linkedList L3;
+    // ALGORITMA
+    CreateList(&L3);
+    p1 = first(L1);
+    p2 = first(L2);
+    while (p1 != NULL) {
+        insertLast(&L3, info(p1));
+        p1 = next(p1);
+    }
+    while (p2 != NULL) {
+        insertLast(&L3, info(p2));
+        p2 = next(p2);
+    }
+    return L3;
+}
---
+++ linkedlist/linkedlist.h
@@ -0,0 +1,94 @@
+// File: linkedlist.h 
+// Berisi definisi dan primitif struktur list berkait 
+// List dinyatakan melalui alamat elemen pertamanya 
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "boolean.h"
+#include <stdio.h>
+
+// DEKLARASI TIPE   
+typedef int infotype;
+typedef struct tnode* Address;
+typedef struct tnode {
+    infotype info;
+    Address next;
+} Node;
+typedef Address linkedList;
+
+// KAMUS UMUM
+#define IDX_UNDEF -1 // Indeks tidak terdefinisi
+
+// SELEKTOR 
+#define info(L) (L)->info
+#define next(L) (L)->next
+#define first(L) (L)
+
+// KONSTRUKTOR 
+void CreateList(linkedList *L);
+// I.S. L sembarang 
+// F.S. L terdefinisi sebagai list kosong, L = NULL 
+
+Address newNode(infotype val);
+// Mengalokasikan val sebagai info dari node baru dan mengembalikan alamatnya
+
+// OPERASI PADA LIST 
+boolean isEmpty(linkedList L);
+// Mengirimkan true jika L kosong, false jika tidak
+
+infotype getElmt(linkedList L, int idx);
+// Mengirimkan nilai elemen L pada indeks ke-idx
+
+void setElmt(linkedList *L, int idx, infotype val);
+// I.S. L terdefinisi, idx adalah indeks yang terdefinisi pada L
+// F.S. Nilai elemen L pada indeks ke-idx adalah 
+
+int indexOf(linkedList L, infotype val);
+// Mencari letak val dalam list dan mengirimkan indeksnya 
+
+int length(linkedList L);
+// Mengirimkan panjang/banyak elemen list L 
+
+void insertFirst(linkedList *L, infotype val);
+// I.S. List L terdefinisi 
+// F.S. val adalah elemen pertama list yang baru; 
+// list L dinyatakan sebagai address dari val
+// jumlah elemen list L bertambah satu 
+
+void insertAt(linkedList *L, int idx, infotype val);
+// I.S. List L terdefinisi 
+// F.S. val adalah elemen list pada indeks ke-idx  yang baru; 
+// jumlah elemen list L bertambah satu
+
+void insertLast(linkedList *L, infotype val);
+// I.S. List L terdefinisi 
+// F.S. val adalah elemen terakhir list yang baru; 
+// jumlah elemen list L bertambah satu
+
+void deleteFirst(linkedList *L, infotype *val);
+// I.S. List L terdefinisi dan tidak kosong 
+// F.S. Elemen pada indeks pertama list dihapus,
+// val adalah nilai yang dihapus dari list 
+
+void deleteAt(linkedList *L, int idx, infotype *val);
+// I.S. List L terdefinisi dan tidak kosong 
+// F.S. Elemen pada indeks ke-idx list dihapus,
+// val adalah nilai yang dihapus dari list 
+
+void deleteLast(linkedList *L, infotype *val, int idx);
+// I.S. List L terdefinisi dan tidak kosong 
+// F.S. Elemen pada indeks terakhir list dihapus,
+// val adalah nilai yang dihapus dari list 
+
+void displayList(linkedList L);
+// I.S. List L terdefinisi, bisa kosong
+// F.S. Elemen list ditampilkan dengan format [e1,e2,...,en]
+
+linkedList concat(linkedList L1, linkedList L2);
+// I.S. List L1 dan L2 terdefinisi 
+// F.S. LOUt terdefinisi sebagai list hasil penempelan L1 dengan L2 
+
+
+
+#endif
