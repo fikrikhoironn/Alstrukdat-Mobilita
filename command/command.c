942e2d3acb76ded6137132231646d3e73dfba2db
chryes220 <christinehutabarat12@gmail.com>
2021-11-08 17:56:27 +0700

    (develop) add TO_DO and IN_PROGRESS commands

--- command/command.c
+++ command/command.c
@@ -27,3 +27,71 @@ jika ada, pick up masukkan ke bag dan update in progress list */
     
 
 }
+
+void TO_DO (linkedList todo) {
+    // KAMUS
+    Address p;
+    int i;
+
+    // ALGORITMA
+    if (!isListEmpty(todo)) {
+        p = first(todo);
+        i = 1;
+
+        printf("Pesanan pada To Do List:\n");
+        while (p != NULL) {
+            printf("%d. %c -> %c ", i, pickUp(info(p)), dropOff(info(p)));
+            if (typeItem(info(p)) == 'N') {
+                printf("(Normal Item)\n");
+            }
+            else if (typeItem(info(p)) == 'P') {
+                printf("(Perishable Item)\n");
+            }
+            else if (typeItem(info(p)) == 'H') {
+                printf("(Heavy Item)\n");
+            }
+            else if (typeItem(info(p)) == 'V') {
+                printf("(VIP Item)\n");
+            }
+            
+            p = next(p);
+            i++;
+        }
+    }
+    else {
+        printf("To Do List kosong.\n");
+    }
+}
+
+void IN_PROGRESS (linkedList inprogress) {
+    // KAMUS
+    Address p;
+    int i;
+
+    // ALGORITMA
+    if (!isListEmpty(inprogress)) {
+        p = first(inprogress);
+        i = 1;
+
+        printf("Pesanan yang sedang diantarkan:\n");
+        while (p != NULL) {
+            printf("%d. ");
+            if (typeItem(info(p)) == 'N') {
+                printf("Normal Item ");
+            }
+            else if (typeItem(info(p)) == 'P') {
+                printf("Perishable Item ");
+            }
+            else if (typeItem(info(p)) == 'H') {
+                printf("Heavy Item ");
+            }
+            else if (typeItem(info(p)) == 'V') {
+                printf("VIP Item ");
+            }
+            printf("(Tujuan: %c)\n", dropOff(info(p)));
+
+            p = next(p);
+            i++;
+        }
+    }
+}
