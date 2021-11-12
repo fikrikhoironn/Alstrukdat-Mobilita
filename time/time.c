7223e4a4db8ca1bdfc16200d3150eccf28074cd2
fikrikhoironn <fikrikf470@gmail.com>
2021-11-12 20:56:51 +0700

    develop: add driver time and change minor in time

--- time/time.c
+++ time/time.c
@@ -1,27 +1,36 @@
 #include "time.h"
+#include <math.h>
+#include <stdio.h>
 
 void createTime(time *t){
     currentTime(*t) = 0;
     speedBoost(*t) = 0;
     heavyItem(*t) = 0;
-    moveCount(*t) = 0;
+    currentMoney(*t) = 0;
+    returnToSender(*t) = 0;
 }
 
 void incrementTime(time *t){
-    if(heavyItem(*t) > 0){ // 
+    if(speedBoost(*t) > 0 && heavyItem(*t) == 0){
+        speedBoost(*t) -= 1;
+        currentTime(*t) += 0.5;
+    }
+    else if(heavyItem(*t) > 0){ // 
         currentTime(*t) += heavyItem(*t) +1;
         speedBoost(*t) = 0; //reset speedBoost
     }
-    else if(speedBoost(*t) > 0 && heavyItem(*t) == 0){
-        speedBoost(*t) -= 1;
-        if (moveCount(*t) == 2 && speedBoost(*t) > 0){
-            currentTime(*t) += 1;
-            moveCount(*t) = 0;
-        }
-    }
+    
     else{
         currentTime(*t) += 1;
+        
     }
+}
+
+void printCurrentTime(time t){
+    float currentTimeFloat = floor(currentTime(t));
+    int my_var = (int)currentTimeFloat;
+    printf("%d\n", my_var);
+    // printf("%f\n", currentTimeFloat);
 }
 
 void addTime(time *t, int amount){
@@ -32,20 +41,20 @@ void subtractTime(time *t, int amount){
     currentTime(*t) -= amount;
 }
 
-void addHeavyItem(time *t){
-    heavyItem(*t) += 1;
+void addSpeedBoost(time *t){
+    speedBoost(*t) += 10;
 }
 
-void decrementHeavyItem(time *t){
-    heavyItem(*t) -= 1;
+void addHeavyItem(time *t, int amount){
+    heavyItem(*t) += amount;
+}
+
+void subtractHeavyItem(time *t, int amount){
+    heavyItem(*t) -= amount;
 }
 
 void resetHeavyItem(time *t){
     heavyItem(*t) = 0;
-}
-
-void initMoveCount(time *t){
-    moveCount(*t) = 0;
 }
 
 void initCurrentMoney(time *t){
