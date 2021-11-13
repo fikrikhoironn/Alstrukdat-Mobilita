a5eb0a62a009e6c6d8ec2ac81cfa003377b78f11
chryes220 <christinehutabarat12@gmail.com>
2021-11-13 12:22:00 +0700

    (develop) add DROP_OFF command

--- command/command.h
+++ command/command.h
@@ -9,11 +9,19 @@
 #include "boolean.h"
 #include "../stack/stack.h"
 #include "../linkedlist/linkedlist.h"
+#include "../ability/ability.h"
 
 
 
 void TO_DO (linkedList todo);
 
 void IN_PROGRESS (linkedList inprogress);
+
+void DROP_OFF (linkedList *inprogress, stack *tas, person *mobita, time *t, map *map);
+// Diasumsikan terdapat adt person bernama mobita yang memiliki komponen money
+// Diasumsikan terdapat adt map
+// Memindahkan item dari in progress list dan melakukan pop item dari tas
+
+
 #endif /* command_H */
 
