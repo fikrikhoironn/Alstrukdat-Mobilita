a43f877abde31d55f5fa1c21b0efc4d832e02bc5
mrfirdauss-20 <16520456@std.stei.itb.ac.id>
2021-11-13 13:03:03 +0700

    Revert "(develop) call activateReturnToSender after dropping VIP item off"
    This reverts commit fea60c9407ee8d0b70bad73196785c2f804f4fd8.

--- command/command.c
+++ command/command.c
@@ -75,7 +75,7 @@ void IN_PROGRESS (linkedList inprogress) {
     }
 }
 
-void DROP_OFF (linkedList *todo, linkedList *inprogress, stack *tas, person *mobita, time *t, map *map) {
+void DROP_OFF (linkedList *inprogress, stack *tas, person *mobita, time *t, map *map) {
     // KAMUS
     item delVal;
 
@@ -108,7 +108,7 @@ void DROP_OFF (linkedList *todo, linkedList *inprogress, stack *tas, person *mob
             }
             else if (isVIPItem(delVal)) {
                 *mobita.money += 600;
-                activateReturnToSender(tas, todo, inprogress, t);
+                activateReturnToSender(); // Diasumsikan ada
                 printf("Pesanan VIP Item berhasil diantarkan\n");
                 printf("Uang yang didapatkan : 600 Yen\n");
                 printf("Anda memperoleh ability Return To Sender\n");
