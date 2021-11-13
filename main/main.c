07f769f7d920b2d44e38cebe2c0fff231160b7a9
mrfirdauss-20 <16520456@std.stei.itb.ac.id>
2021-11-13 12:59:40 +0700

    start main

---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include  "mobilita.h"
+
+int main(){
+    int command;
+    do{
+        scanf("%d",&command);
+    }while(command != 1 || command != 2 || command !=3);
+    if(command==1){
+            scanf("%s[^\n]",);
+            start_game();
+            readConfigFiles();
+    }else if(command ==2){
+    
+    }else{
+     //print   
+    }
+
+    while (!isEmpty)
+    {
+        /* code */
+    }
+    
+}
---
+++ main/mobilita.h
@@ -0,0 +1,24 @@
+#include "boolean.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../mesinKata/mesin.h"
+#include "../command/command.h"
+#include "../queue/queue.h"
+#include "../arraydin/arraydin.h"
+#include "../time/time.h"
+#include "../stack/stack.h"
+#include "../point/point.h"
+#include "../matrix/matrix.h"
+#include "../locations/locations.h"
+#include "../linkedlist/linkedlist.h"
+#include "../item/item.h"
+#include "../arraystatis/arraystatis.h"
+#include "../ability/ability.h"
+
+typedef struct mobilita{
+    
+}mobilita;
+
+void start_game();
+/* initiate ADT to start the game, set everyting at zero point before start the game*/
+
