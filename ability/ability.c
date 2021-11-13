32073f1729c75a47af0c50c66c67846935528deb
fikrikhoironn <fikrikf470@gmail.com>
2021-11-13 09:40:30 +0700

    develop: ability beres

--- ability/ability.c
+++ ability/ability.c
@@ -10,3 +10,13 @@ void increaseCapactiy(stack *bag){
     }
 }
 
+void activateReturnToSender(stack *bag, linkedList *toDoList, linkedList *inProgressList, time t){
+    item val;
+    val = getElmtList(*inProgressList, 0);
+    if (typeItem(val) != 'V' &&  returnToSender(t) > 0){
+        popBag(bag, &val);
+        deleteFirstList(inProgressList, &val);
+        insertLastList(toDoList, val);
+    }
+}
+
--- ability/ability.h
+++ ability/ability.h
@@ -1,6 +1,11 @@
 #include "../time/time.h"
+#include "../stack/stack.h"
+#include "../linkedList/linkedlist.h"
+#include "../item/item.h"
 #include "boolean.h"
 
 void activateSpeedBoost(time *t);
 
 void increaseCapactiy(stack *bag);
+
+void activateReturnToSender(stack *bag, linkedList *toDoList, linkedList *inProgressList, time t);
